515e0f7c21936341959af7f31a1992265b573666
Jeffery McLean <jeffery.mclean@gmail.com>
2013-11-27 19:23:29 +0000

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RottenMangoesTest::Application.config.secret_key_base = '41637413d9eb378f16ccb4c9d8e165403fdb1286cba5b3f6ce4d78f7038db6e4b97b5a5a220fae36057996cc86166c1a4ea6ec2fd8e44c7234296df15145d0e7'
