9d3a9517179836afc2d20afcdb5cbc9e362e5bf4
Jeffery McLean <jeffery.mclean@gmail.com>
2013-11-28 03:16:33 +0000

    write CRUD actions and movie_params on movie controller

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,55 +1,53 @@
 class MoviesController < ApplicationController
- 
+
   def index
-  	@movies = Movie.all 
+    @movies = Movie.all
   end
 
-  def show
-  	@movie = Movie.find(params[:id)])
+  def show 
+    @movie = Movie.find(params[:id])
   end
 
-  def new
-  	@movie = Movie.new
+  def new 
+    @movie = Movie.new
   end
 
   def edit
-  	@movie = Movie.find(params[:id])
+    @movie = Movie.find(params[:id])
   end
 
-  def create 
-  	@movie = Movie.new(movie_params)
-  	
-  	if @movie.save
-  		redirect_to movies_path
-  	else 
-  		render :new
-  	end
+  def create
+    @movie = Movie.new(movie_params)
+  
+    if @movie.save
+      redirect_to movies_path 
+    else
+      render :new  # am i calling new from line 11?
+    end
   end
 
-  def update 
-  	@movie = Movie.find(params[:id])
-
-  	if @movie.update_attributes(movie_params)
-  		redirect_to movies_path(@movie)
-  	else 
-  		render :edit
-  	end
+  def update
+    @movie = Movie.find(params[:id])
+  
+    if @movie.update_attributes(movie_params) # what's this line doing?
+      redirect_to movie_path(@movie) #diff bet. here and line 23
+    else 
+      render :new
+    end
   end
 
   def destroy 
-  	@movie = Movie.find(params[:id])
-  	@movie.destroy
-  	redirect_to movies_path
+    @movie = Movie.find(params[:id])
+    @movie.destroy
+    redirect_to movies_path 
   end
 
   protected
 
-  def movie_params
-  	params.require(:movie).permit(
-  		:title, :release_date, :director, :runtime_in_minutes, :poster_image_url, :description
-  	)
+  def movie_params 
+    params.require(:movie).permit(
+      :title, :release_date, :director, :runtime_in_minutes, :poster_image_url, :description, 
+    )
   end
-
-end
 
 end
